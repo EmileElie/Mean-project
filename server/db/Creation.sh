bc23a4a5ae84a85ee5c4ba2a565de41ce13a5eaa
EmileElie <emile.youssef94@gmail.com>
2018-11-22 15:34:27 +0100

    update db

--- server/db/Creation.sh
+++ server/db/Creation.sh
@@ -1,9 +1,9 @@
 
-echo "Inserting.. Membres"
-mongoimport --db dbsite --collection Membres --drop --file Membres.json --jsonArray 
-mongoimport --db dbsite --collection Biens --drop --file Biens.json --jsonArray 
-mongoimport --db dbsite --collection Services --drop --file Services.json --jsonArray 
-mongoimport --db dbsite --collection Utilisations --drop --file Utilisations.json --jsonArray
-mongoimport --db dbsite --collection Disponibilites --drop --file Disponibilites.json --jsonArray
-mongoimport --db dbsite --collection DescriptifBiens --drop --file DescriptifBiens.json --jsonArray
-mongoimport --db dbsite --collection DescriptifServices --drop --file DescriptifServices.json --jsonArray
+echo "Inserting.. Collections"
+mongoimport --db mean --collection Membres --drop --file Membres.json --jsonArray 
+mongoimport --db mean --collection Biens --drop --file Biens.json --jsonArray 
+mongoimport --db mean --collection Services --drop --file Services.json --jsonArray 
+mongoimport --db mean --collection Utilisations --drop --file Utilisations.json --jsonArray
+mongoimport --db mean --collection Disponibilites --drop --file Disponibilites.json --jsonArray
+mongoimport --db mean --collection DescriptifBiens --drop --file DescriptifBiens.json --jsonArray
+mongoimport --db mean --collection DescriptifServices --drop --file DescriptifServices.json --jsonArray
