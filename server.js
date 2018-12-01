b18c208509d2ad5e7fa51256c2d964e0bc5e8910
EmileElie <emile.youssef94@gmail.com>
2018-12-01 12:07:22 +0100

    update

---
+++ server.js
@@ -0,0 +1,40 @@
+"use strict"
+
+var assert      = require("assert");
+var express     = require('express');
+var app         = express();
+var MongoClient = require("mongodb").MongoClient;
+var url         = "mongodb://localhost:27017";
+
+var port = 8888;
+
+MongoClient.connect(url, {useNewUrlParser: true}, (err, client) => {
+	let db = client.db("mean");
+	assert.equal(null, err);
+
+	app.get("/membres", (req, res) => {
+		db.collection("membres").find().toArray((err, documents) => {
+			res.setHeader("Access-Control-Allow-Origin", "*");
+			res.setHeader("Content-type", "application/json");
+			console.log("/membres : ");
+			for (let doc of documents)
+				console.log(doc);
+			res.end(JSON.stringify(documents));
+		});
+	});
+
+	app.get("/membres/:id", (req, res) => {
+		db.collection("membres").find(req.params.id).toArray((err, documents) => {
+			res.setHeader("Access-Control-Allow-Origin", "*");
+			res.setHeader("Content-type", "application/json");
+			console.log("/membres : ");
+			for (let doc of documents)
+				console.log(doc);
+			res.end(JSON.stringify(documents));
+		});
+	});
+});
+
+app.listen(port, function(){
+	console.log('Server started on port ' + port);
+});
--- server/db/creation.sh
+++
@@ -1,9 +0,0 @@
-
-echo "Inserting.. Collections"
-mongoimport --db mean --collection membres --drop --file membres.json --jsonArray 
-mongoimport --db mean --collection biens --drop --file biens.json --jsonArray 
-mongoimport --db mean --collection services --drop --file services.json --jsonArray 
-mongoimport --db mean --collection utilisations --drop --file utilisations.json --jsonArray
-mongoimport --db mean --collection disponibilites --drop --file disponibilites.json --jsonArray
-mongoimport --db mean --collection descriptifBiens --drop --file descriptifBiens.json --jsonArray
-mongoimport --db mean --collection descriptifServices --drop --file descriptifServices.json --jsonArray
